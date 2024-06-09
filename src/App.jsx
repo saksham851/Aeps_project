7390ce304a790ba039f70a95bc82f78736ca00ce
Saksham <121371714+saksham851@users.noreply.github.com>
2024-06-09 10:55:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/services" element={<Services/>}/>
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
